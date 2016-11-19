f1b5ed38d4534debd4a799c8f9ccd9344ce19e41
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-11-19 13:04:04 +0200

    remove unnecessary files from boilerplate

--- api/actions/index.js
+++ api/actions/index.js
@@ -1,9 +1,6 @@
-export loadInfo from './loadInfo';
 export loadAuth from './loadAuth';
 export login from './login';
 export logout from './logout';
-export * as widget from './widget/index';
-export * as survey from './survey/index';
 export createNewUser from './signUp';
 export {getCards, addNewCard, updateCard, deleteCard} from './cards';
 export * as transactionActions from './transaction';
--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
 import auth from './auth';
-import counter from './counter';
 import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
 import signUp from './signUp';
 import welcomeButtons from './welcomeButtons';
 import cards from './cards';
@@ -18,13 +14,6 @@ export default combineReducers({
   reduxAsyncConnect,
   auth,
   form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
-  info,
-  widgets,
   signUp,
   welcomeButtons,
   cards
