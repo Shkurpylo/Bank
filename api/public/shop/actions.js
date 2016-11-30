20d14eafc4496c9628b0b9e86d76a2a9d4a4ad1c
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-12-01 01:59:11 +0200

    add paymentOfBuying to public API

--- api/public/shop/actions.js
+++ api/public/shop/actions.js
@@ -1,32 +1,41 @@
 import mongoose from 'mongoose';
 import { Transaction } from '../../models';
 import { User } from '../../models';
+import { hideNumber } from './helpers';
 
 
-export function getUserCards() {
+export function getUserCards(req) {
   return new Promise((resolve, reject) => {
-    const ownerId = mongoose.Types.ObjectId('583c89cee9b0ff599bfda427'); // eslint-disable-line new-cap
-    const cards = User.findById(ownerId).distinct('cards');
-    // TODO: add balance check
-    if (!cards) {
-      reject('empty cards list');
-    }
-    resolve(cards);
+    const ownerId = req.body.id;
+    User.findById(ownerId).distinct('cards', (err, array) => {
+      if (err) reject(err);
+      console.log(JSON.stringify(array));
+      const cards = array.map(function(card) {
+        const cardObj = {};
+        cardObj._id = card._id;
+        cardObj.name = card.name;
+        cardObj.number = hideNumber(card.number);
+        return cardObj;
+      });
+      resolve(cards);
+    });
   });
 }
+
 
 export function getUserId(req) { // post
   const email = req.body.email;
   const password = req.body.password;
   return new Promise((resolve, reject) => {
     User.findOne({ 'email': email }, (err, user) => {
-      if (user === null) {
+      console.log(JSON.stringify(user));
+      if (!user) {
         reject('wrong email');
         return null;
       }
+      if (typeof password !== 'string') reject('password must be string');
       if (!user.validPassword(password)) {
         reject('wrong pass');
-        console.log(req.body.email + ' pass: ' + req.body.password);
         return null;
       }
       resolve(user._id);
@@ -36,18 +45,33 @@ export function getUserId(req) { // post
 }
 
 export function paymentOfBuying(req) {
-  const staticReceiver = mongoose.Types.ObjectId('583627c13b8ce854fa11e21e'); // eslint-disable-line new-cap
+  const receiverId = mongoose.Types.ObjectId('583627c13b8ce854fa11e21e'); // eslint-disable-line new-cap
+  const receiverCardId = mongoose.Types.ObjectId('583f3fcecefa2b0db2b77d26'); // eslint-disable-line new-cap
+  const receiverCardNumber = 4019975145166519;
   return new Promise((resolve, reject) => {
-    const transaction = new Transaction({
-      sender: req.body.sender,
-      receiver: staticReceiver,
-      amount: req.body.amount,
-      date: new Date(),
+    User.findOne({ 'cards._id': req.body.id }, 'cards': { $elemMatch: { '_id': req.body.id } }, (err, user) => {
+      const card = user.cards[0];
+      const transaction = new Transaction({
+        message: 'payment for shopping in TrueShop1997',
+        sender: {
+          userId: card.owner,
+          cardId: req.body.id,
+          cardNumber: card.number
+        },
+        receiver: {
+          userId: receiverId,
+          cardId: receiverCardId,
+          cardNumber: receiverCardNumber
+        },
+        amount: req.body.amount,
+        date: new Date(),
+      });
+      transaction.save((saveErr, result) => {
+        if (saveErr) reject(saveErr);
+        if (result) {
+          resolve('payment success');
+        }
+      });
     });
-    const result = transaction.save();
-    if (!result) {
-      reject('err');
-    }
-    resolve('ok');
   });
 }
