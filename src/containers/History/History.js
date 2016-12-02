05f936869a139af8f5760d9f83c65bab745b1044
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-12-02 21:04:29 +0200

    add dateFormat func in History.js

--- src/containers/History/History.js
+++ src/containers/History/History.js
@@ -6,6 +6,21 @@ import { initializeWithKey } from 'redux-form';
 import * as transactionsActions from 'redux/modules/transaction';
 import { isLoaded, getTransactions as loadTransactions } from 'redux/modules/transaction';
 
+const dateFormat = (date) => {
+  const formatingDate = new Date(date);
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    // weekday: 'long',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+  return (formatingDate.toLocaleString('en-US', options));
+};
+
 @asyncConnect([{
   deferred: true,
   promise: ({ store: { dispatch, getState } }) => {
@@ -21,7 +36,7 @@ import { isLoaded, getTransactions as loadTransactions } from 'redux/modules/tra
     transactions: state.transaction.transactions,
     error: state.transaction.error,
     loaded: state.transaction.loaded,
-  }), {...transactionsActions, initializeWithKey})
+  }), {...transactionsActions, initializeWithKey })
 export default class History extends Component {
   static propTypes = {
     transactions: PropTypes.array
@@ -37,11 +52,35 @@ export default class History extends Component {
         <Helmet title="transactions"/>
         <h1>History</h1>
       </div>
+
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <div className="btn-group btn-group-justified" role="group" aria-label="...">
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">Sending</button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">All</button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">Reciving</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+        <h3>date picker</h3>
+        </div>
+      </div>
+      <div className="row">
+      <div className="col-md-10 col-md-offset-1 panel panel-default">
     {transactions && transactions.length &&
             <table className="table table-hover">
               <thead>
               <tr>
                 <th className={styles.colorCol}>Date</th>
+                <th className={styles.colorCol}>Massage</th>
                 <th className={styles.sprocketsCol}>Sender</th>
                 <th className={styles.ownerCol}>Receiver</th>
                 <th className={styles.buttonCol}>Amount</th>
@@ -51,14 +90,17 @@ export default class History extends Component {
               {
                 transactions.map((transaction) =>
                   <tr key={transaction._id}>
-                    <td className={styles.buttonCol} >{transaction.date}</td>
-                    <td className={styles.idCol} >{transaction.sender}</td>
-                    <td className={styles.colorCol} >{transaction.receiver}</td>
+                    <td className={styles.buttonCol} >{dateFormat(transaction.date)}</td>
+                    <td className={styles.colorCol} >{transaction.message}</td>
+                    <td className={styles.idCol} >{transaction.sender.cardNumber}</td>
+                    <td className={styles.colorCol} >{transaction.receiver.cardNumber}</td>
                     <td className={styles.ownerCol} >{transaction.amount}</td>
                   </tr>)
               }
               </tbody>
             </table> }
+          </div>
+          </div>
 
        </div>
 
