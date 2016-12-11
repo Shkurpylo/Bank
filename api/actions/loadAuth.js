baf10617256e832740f165cf1c79bfde0e2d555f
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-12-11 21:49:42 +0200

    fix bug with passport implementation

--- api/actions/loadAuth.js
+++ api/actions/loadAuth.js
@@ -2,6 +2,5 @@ export default function loadAuth(req) {
   if ('passport' in req.session) {
     return Promise.resolve(req.session.passport.user || null);
   }
-  return null;
-  // return Promise.resolve(req.user || null);
+  return Promise.resolve(null);
 }
