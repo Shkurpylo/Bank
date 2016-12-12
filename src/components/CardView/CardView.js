c6390357caa41963ab2a1bbddfe9bde4626fe4cc
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-12-12 15:34:29 +0200

    add cardView style

--- src/components/CardView/CardView.js
+++ src/components/CardView/CardView.js
@@ -12,6 +12,13 @@ const dateFormat = (date) => {
   };
   return (formatingDate.toLocaleString('en-US', options));
 };
+const numberFormat = (number) => {
+  const numberArr = ('' + number).split('');
+  numberArr.splice(4, 0, '\xa0\xa0');
+  numberArr.splice(9, 0, '\xa0\xa0');
+  numberArr.splice(14, 0, '\xa0\xa0');
+  return numberArr.join('');
+};
 
 @connect((state) => ({
   card: state.cards.card,
@@ -47,6 +54,7 @@ export default class CardView extends Component {
   };
 
   render() {
+    // const creditCard = require('./card.png');
     const handleEdit = (card) => {
       const { editStart } = this.props; // eslint-disable-line no-shadow
       return () => editStart(String(card._id));
@@ -64,17 +72,15 @@ export default class CardView extends Component {
       editing,
       // formKey,
       updateCard,
-      saveError: {[card._id]: saveError },
+      saveError: {
+        [card._id]: saveError
+      },
       values
     } = this.props;
-    const style = require('./CardView.scss');
+    const styles = require('./CardView.scss');
     return (
-      <div className="panel panel-info" id={style.panel}>
-      <div className="panel-heading">Card</div>
-      <div className="row">
-        <div className="col-md-4">
-        </div>
 
+      <div className={styles.cardView + ' col-md-12'}>
         {editing[card._id] ?
           <div formKey={card.name}>
             <input type="text" key={String(card._id)} value={card.name} className="form-control" {...name}/>
@@ -82,70 +88,49 @@ export default class CardView extends Component {
                     onClick={() => editStop(String(card._id))}
                     disabled={submitting}>
               <i className="fa fa-ban"/> Cancel
-            </button>
-            <button className="btn btn-success"
-                    onClick={handleSubmit(() => updateCard(values, card._id)
-                      .then(result => {
-                        console.log('in handleSubmit: ' + values.name);
-                        if (result && typeof result.error === 'object') {
-                          return Promise.reject(result.error);
-                        }
-                      })
-                    )}
-                    disabled={pristine || invalid || submitting}>
-              <i className={'fa ' + (submitting ? 'fa-cog fa-spin' : 'fa-cloud')}/> Save
-            </button>
-            {saveError && <div className="text-danger">{saveError}</div>}
+              </button>
+              <button className="btn btn-success"
+                      onClick={handleSubmit(() => updateCard(values, card._id)
+                        .then(result => {
+                          console.log('in handleSubmit: ' + values.name);
+                          if (result && typeof result.error === 'object') {
+                            return Promise.reject(result.error);
+                          }
+                        })
+                      )}
+                      disabled={pristine || invalid || submitting}>
+                <i className={'fa ' + (submitting ? 'fa-cog fa-spin' : 'fa-cloud')}/> Save
+              </button>
+              {saveError && <div className="text-danger">{saveError}</div>}
+            </div>
+            :
+          <div className={styles.cardName}>
+            {card.name}
+          <button className="btn btn-link" onClick={handleEdit(card)}
+          >Change Name</button>
           </div>
-          :
-        <div className="col-md-6">
-         {card.name}
-        <button className="btn btn-link" onClick={handleEdit(card)}
-        >Change Name</button>
-        </div>
-        }
-      </div>
-      <div className="row">
-        <div className="col-md-4">
-          Number:
-        </div>
-        <div className="col-md-6">
-          {card.number}
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-4">
-          cvv:
-        </div>
-        <div className="col-md-6">
-          {card.cvv}
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-4">
-          explDate:
+          }
+
+
+         <div className={styles.bgimg}>
+           <p> </p>
+           <div className={styles.cardNumber}>{numberFormat(card.number)}</div>
+           <div className={styles.explDate + ' col-md-5'}>{dateFormat(card.explDate)}</div>
+           <div className={styles.cvv}> {card.cvv} </div>
+           <div className={styles.balance}>
+             Balance: {card.balance + '$'}
+           </div>
+           <div className={styles.delButton}>
+            <button className="btn btn-danger"
+                    onClick={() => deleteCard(card._id)
+                      .then(getCards())}>
+                      <i className="fa fa-trash" aria-hidden="true"></i>
+                       Delete Card</button>
           </div>
-        <div className="col-md-6">
-           {dateFormat(card.explDate)}
-        </div>
+         </div>
+
       </div>
-      <div className="row">
-        <div className="col-md-4">
-          Balance:
-        </div>
-        <div className="col-md-6">
-          {card.balance + '$'}
-        </div>
-      </div>
-      <div className="row">
-        <div className={style.buttons}>
-          <button className="btn btn-danger"
-                  onClick={() => deleteCard(card._id)
-                    .then(getCards())}>
-                    Delete Card</button>
-        </div>
-      </div>
-    </div>
+
     );
   }
 }
