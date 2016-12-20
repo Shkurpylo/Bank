7cd8ae5d8b8dd7cedbee64275c0f990758efc27c
Anatolii Shkurpylo <anatolii.shkurpylo@gmail.com>
2016-12-21 00:01:42 +0200

    fix SignUp style

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -47,17 +47,25 @@ export default class SignUpForm extends Component {
     const styles = require('./SignUpForm.scss');
     const renderInput = (field, label, showAsyncValidating, hide) =>
       <div className={'form-group' + (field.error && field.touched ? ' has-error' : '')}>
-        <div>
+        <div className="row">
           {showAsyncValidating && asyncValidating && <i className={'fa fa-cog fa-spin ' + styles.cog}/>}
-          <input type={hide ? 'password' : 'text'} className="form-control" placeholder={label} id={field.name} {...field}/>
-          {field.error && field.touched && <div className="text-danger">{field.error}</div>}
+          <div className={styles.inputBox + ' col-md-9'}>
+            <input
+              type={hide ? 'password' : 'text'}
+              className="form-control"
+              placeholder={label} id={field.name}
+              {...field}/>
+          </div>
+          <div className={styles.textDanger + ' col-md-3'}>
+          {field.error && field.touched && <div className={' text-danger'}>{field.error}</div>}
+          </div>
         </div>
       </div>;
 
     return (
-      <div className="col-sm-4 col-md-offset-4">
+      <div className={styles.form + ' col-sm-5 col-md-offset-4'}>
       <Helmet title="signUp"/>
-        <h3>Sign Up</h3>
+        <h3 className={styles.titleSignUp}>Sign Up</h3>
         <form className="form-group">
           {renderInput(name, 'Name')}
           {renderInput(lastName, 'Last Name')}
@@ -65,11 +73,11 @@ export default class SignUpForm extends Component {
           {renderInput(pass, 'Password', false, true)}
           {renderInput(confirmPass, 'Confirm Password', false, true)}
           <div className="form-group">
-            <div className="col-sm-offset-1 col-sm-10">
+            <div className={styles.buttons + ' col-sm-offset-1 col-sm-10'}>
               <button className="btn btn-success" onClick={handleSubmit(() => (createNewUser(values)))}>
                 <i className="fa fa-paper-plane"/> Submit
               </button>
-              <button className="btn btn-warning" onClick={resetForm} style={{marginLeft: 15}}>
+              <button className="btn btn-warning" onClick={resetForm} style={{marginLeft: 15, marginRight: 50}}>
                 <i className="fa fa-undo"/> Reset
               </button>
             </div>
